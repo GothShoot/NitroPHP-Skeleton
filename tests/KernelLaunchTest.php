689ecd46ce1d431da7e3d0fa627e239394cd11f1
GothShoot <romuald.mocq@yahoo.com>
2020-06-01 03:23:55 +0200

    Fix kernel instantiation tests

--- tests/KernelLaunchTest.php
+++ tests/KernelLaunchTest.php
@@ -5,8 +5,13 @@ use PHPUnit\Framework\TestCase;
 
 final class KernelLaunchTest extends TestCase
 {
-    public function testCanBeCreatedFromValidEmailAddress(): void
+    public function testCanBeInstanciedInProd(): void
     {
-        $this->assertInstanceOf(App\Kernel::class, $kernel = new App\Kernel);
+        $this->assertInstanceOf(App\Kernel::class, $kernel = new App\Kernel('prod', false) );
+    }
+
+    public function testCanBeInstanciedInDev(): void
+    {
+        $this->assertInstanceOf(App\Kernel::class, $kernel = new App\Kernel('dev', true) );
     }
 }
